f8e1f534ae209074a4c37ca692d205f7d0d8fc1f
Ahmad Rizki Maulana <ahmadrizkimaulana666@gmail.com>
2025-05-15 16:09:55 +0700

    done

--- Project_6/attention/mask.py
+++ Project_6/attention/mask.py
@@ -45,8 +45,15 @@ def get_mask_token_index(mask_token_id, inputs):
     Return the index of the token with the specified `mask_token_id`, or
     `None` if not present in the `inputs`.
     """
+    if mask_token_id is None:
+        return None
+    
+    for i, token_id in enumerate(inputs["input_ids"][0]):
+        if token_id == mask_token_id:
+            return i
+    
     # TODO: Implement this function
-    raise NotImplementedError
+    # raise NotImplementedError
 
 
 
@@ -55,8 +62,12 @@ def get_color_for_attention_score(attention_score):
     Return a tuple of three integers representing a shade of gray for the
     given `attention_score`. Each value should be in the range [0, 255].
     """
+    attention_score = int(float(attention_score) * 255)
+    result = (attention_score, attention_score, attention_score)
+    return result
+
     # TODO: Implement this function
-    raise NotImplementedError
+    # raise NotImplementedError
 
 
 
@@ -70,13 +81,24 @@ def visualize_attentions(tokens, attentions):
     include both the layer number (starting count from 1) and head number
     (starting count from 1).
     """
+    layers = len(attentions)
+    heads = len(attentions[0])
+
+    for layer_number in range(layers):
+        for head_number in range(heads):
+            generate_diagram(
+                layer_number + 1,
+                head_number + 1,
+                tokens,
+                attentions[layer_number][head_number]
+            )
     # TODO: Update this function to produce diagrams for all layers and heads.
-    generate_diagram(
-        1,
-        1,
-        tokens,
-        attentions[0][0][0]
-    )
+    # generate_diagram(
+    #     1,
+    #     1,
+    #     tokens,
+    #     attentions[0][0][0]
+    # )
 
 
 def generate_diagram(layer_number, head_number, tokens, attention_weights):
